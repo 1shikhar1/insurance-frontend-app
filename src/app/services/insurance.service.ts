69e73c8a509a500306ba68ee8ade40906e4516fe
Vineet-Singh1 <103960294+Vineet-Singh1@users.noreply.github.com>
2022-06-19 01:57:35 +0530

    update changes

--- src/app/services/insurance.service.ts
+++ src/app/services/insurance.service.ts
@@ -49,4 +49,35 @@ export class InsuranceService {
     });
     return this.http.post(url, data, { headers: httpHeaders });
   }
+  activateInsuranceType(id:number){
+    let token = "Bearer " + localStorage.getItem("token");
+    let url =  "http://localhost:8080/api/v1/customer/"+id+"/activate";
+    return this.http.get<any[]>(url, {
+     headers: new HttpHeaders({
+        'Authorization': token
+      })
+    })
+  }
+
+  deactivateInsuranceType(id:number){
+    let token = "Bearer " + localStorage.getItem("token");
+    let url =  "http://localhost:8080/api/v1/customer/"+id+"/deactivate";
+    return this.http.get<any[]>(url, {
+     headers: new HttpHeaders({
+        'Authorization': token
+      })
+    })
+  }
+
+  deleteInsuranceTypeById(id:number){
+    let token = "Bearer " + localStorage.getItem("token");
+    let url =  "http://localhost:8080/api/v1/customer/"+id+"/delete";
+    return this.http.get<any[]>(url, {
+     headers: new HttpHeaders({
+        'Authorization': token
+      })
+    })
+  }
+
+
 }

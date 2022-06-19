2aa895e3bea502014ce1be38eb2525f2aeec24d5
1shikhar1 <1shikhar1@gmail.com>
2022-06-20 03:23:53 +0530

    update

--- src/app/payment-slip/payment-slip.component.ts
+++ src/app/payment-slip/payment-slip.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataSharingService } from '../service/data-sharing.service';
 
 @Component({
   selector: 'app-payment-slip',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class PaymentSlipComponent implements OnInit {
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private dataSharingService: DataSharingService) { }
 
   ngOnInit(): void {
   }
